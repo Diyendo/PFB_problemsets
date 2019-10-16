afab251c11f5772f83dd0c619ae6ffec29758404
info <info@info27.cshl.edu>
2019-10-16 11:23:22 -0400

    end of Pyhton1 Problem Set

--- day2_Python1/myFirstPythonScript.py
+++ day2_Python1/myFirstPythonScript.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-myname = "Diyendo"
-print("My name is",myname)
 
+import sys
+
+myName = sys.argv [1]
+myFavoriteColor = sys.argv [2]
+myFavoriteActivity = sys.argv [3]
+myfavoriteAnimal = sys.argv [4]
+
+print("My name :", myName, "\nMy favorite color :", myFavoriteColor,"\nMy favorite activity :", myFavoriteActivity, "\nMy favorite animal :", myfavoriteAnimal)
